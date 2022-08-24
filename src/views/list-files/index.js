9abc119f27a0cb517e9c5569d33cfacfdd34b29d
victoriacassolo <vitoria.cassolo@gmail.com>
2022-08-24 15:34:46 -0300

    add list files content

--- src/views/list-files/index.js
+++ src/views/list-files/index.js
@@ -1,44 +1,55 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Table } from 'reactstrap';
 import { api } from '../../services/api';
 
 const ListFiles = () => {
+  const [dados, setDados] = useState();
   const getFile = async () => {
     try {
       const response = await api.get(`/files`);
-      console.log(response);
+      console.log(response.data.files);
+      setDados(response.data.files);
     } catch (error) {
       console.log(error);
     }
   };
-
   useEffect(() => {
     getFile();
-  });
+  }, []);
 
   return (
-    <Table responsive>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>File</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>
-            <span className="align-middle fw-bold">Angular Project</span>
-          </td>
-          <td>Peter Charles</td>
-        </tr>
-        <tr>
-          <td>
-            <span className="align-middle fw-bold">React Project</span>
-          </td>
-          <td>Ronald Frest</td>
-        </tr>
-      </tbody>
-    </Table>
+    <>
+      {!dados ? null : (
+        <Table responsive>
+          <thead>
+            <tr>
+              <th>Hash</th>
+              <th>File Name</th>
+              <th>Created At</th>
+              <th>File Url</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dados.map((data) => (
+              <tr>
+                <td>
+                  <p className="card-text text-wrap">{data.hash}</p>
+                </td>
+                <td>
+                  <p className="card-text text-wrap">{data.file}</p>
+                </td>
+                <td>
+                  <p className="card-text text-wrap">{data.created_at}</p>
+                </td>
+                <td>
+                  <p className="card-text text-wrap">{data.file_url}</p>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
   );
 };
 
