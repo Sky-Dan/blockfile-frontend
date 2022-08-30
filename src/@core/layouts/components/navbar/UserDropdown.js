39ab51bd0d645593c8729ea11dfba93834079b11
victoriacassolo <vitoria.cassolo@gmail.com>
2022-08-30 10:26:59 -0300

    add logout button

--- src/@core/layouts/components/navbar/UserDropdown.js
+++ src/@core/layouts/components/navbar/UserDropdown.js
@@ -2,15 +2,12 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-// ** Custom Components
-import Avatar from '@components/avatar';
-
 // ** Utils
 import { isUserLoggedIn } from '@utils';
 
 // ** Store & Actions
-// import { useDispatch } from 'react-redux'
-// import { handleLogout } from '@store/actions/auth'
+import { useDispatch } from 'react-redux';
+import { handleLogout } from '../../../../redux/auth';
 
 // ** Third Party Components
 import {
@@ -18,15 +15,16 @@ import {
   DropdownMenu,
   DropdownToggle,
   DropdownItem,
+  Button,
 } from 'reactstrap';
-import { User, CheckSquare, Power } from 'react-feather';
+import { User, Power } from 'react-feather';
 
 // ** Default Avatar Image
-// import defaultAvatar from '@src/assets/images/portrait/small/avatar-s-11.jpg';
+// import defaultAvatar from '@src/assets/images/portrait/small/avatar-s-11.jpg'
 
 const UserDropdown = () => {
   // ** Store Vars
-  // const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   // ** State
   const [userData, setUserData] = useState(null);
@@ -39,27 +37,37 @@ const UserDropdown = () => {
   }, []);
 
   //** Vars
-  // const userAvatar = (userData && userData.avatar) || defaultAvatar;
+  // const userAvatar = (userData && userData.avatar) || defaultAvatar
 
   return (
     <UncontrolledDropdown tag="li" className="dropdown-user nav-item">
-      {/* <DropdownToggle href='/' tag='a' className='nav-link dropdown-user-link' onClick={e => e.preventDefault()}>
-        <div className='user-nav d-sm-flex d-none'>
-          <span className='user-name font-weight-bold'>{(userData && userData['username']) || 'Miojo'}</span>
-          <span className='user-status'>{(userData && userData.role) || 'Professor'}</span>
+      <DropdownToggle
+        href="/"
+        tag="a"
+        className="nav-link dropdown-user-link"
+        onClick={(e) => e.preventDefault()}
+      >
+        {/* <Avatar img={userAvatar} imgHeight='40' imgWidth='40' status='online' /> */}
+        <div className="user-nav">
+          {/* <span className='user-name'>&nbsp;&nbsp;&nbsp;&nbsp;{(userData && userData.role) || 'Professor'}</span> */}
+          <Button type="button" color="primary" outline>
+            Opções
+          </Button>
         </div>
-        <Avatar img={userAvatar} imgHeight='40' imgWidth='40' status='online' />
-      </DropdownToggle> */}
-      <DropdownMenu right>
-        <DropdownItem tag={Link} to="/pages/profile">
+      </DropdownToggle>
+      <DropdownMenu end>
+        {/* <DropdownItem tag={Link} to="/pages/profile">
           <User size={14} className="mr-75" />
-          <span className="align-middle">Profile</span>
+          <span className="align-middle"> Editar</span>
         </DropdownItem>
-        <DropdownItem divider />
-        <DropdownItem tag={Link} to="/login">
-          {/* onClick={() => dispatch(handleLogout())} */}
+        <DropdownItem divider /> */}
+        <DropdownItem
+          tag={Link}
+          to="/login"
+          onClick={() => dispatch(handleLogout())}
+        >
           <Power size={14} className="mr-75" />
-          <span className="align-middle">Logout</span>
+          <span className="align-middle"> Sair</span>
         </DropdownItem>
       </DropdownMenu>
     </UncontrolledDropdown>
